dd2d44c9722ad35d0470a145a890a6aa5857bd84
aressss1 <codewithfaizi@gmail.com>
2023-09-01 11:00:53 +0500

    -6

--- components/pro-modal.tsx
+++ components/pro-modal.tsx
@@ -8,9 +8,25 @@ import { Card } from "./ui/card"
 import { cn } from "@/lib/utils"
 import { CheckIcon, LightningBoltIcon } from "@radix-ui/react-icons"
 import { Button } from "./ui/button"
+import axios from "axios"
+import { useState } from "react"
 
 const ProModal = () => {
     const proModal = useProModal()
+    const [loading , setLoading] = useState(false)
+
+    const onSubscribe = async () => {
+        try {
+            setLoading(true)
+            const response = await axios.get("/api/stripe")
+
+            window.location.href = response.data.url
+        } catch (error) {
+            console.log(error , "STRIPE_CLIENT_ERROR")
+        } finally {
+            setLoading(false)
+        }
+    }
 
     return (
         <Dialog open={proModal.isOpen} onOpenChange={proModal.onClose}>
@@ -45,6 +61,7 @@ const ProModal = () => {
                 </DialogHeader>
                 <DialogFooter>
                     <Button
+                        onClick={onSubscribe}
                         size="lg"
                         variant="premium"
                         className="w-full"
