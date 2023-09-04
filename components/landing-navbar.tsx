15ff6d7b21f5bb0f3361b9449b6fae810089c98c
aressss1 <codewithfaizi@gmail.com>
2023-09-04 10:26:33 +0500

    btn test2

--- components/landing-navbar.tsx
+++ components/landing-navbar.tsx
@@ -25,7 +25,7 @@ export const LandingNavbar = () => {
       </Link>
       <div className="flex items-center gap-x-2">
         <Link href={isSignedIn ? "/dashboard" : "/sign-up"}>
-          <Button variant="test1" className="rounded-full">
+          <Button variant="test2" className="rounded-full">
             Get Started
           </Button>
         </Link>
